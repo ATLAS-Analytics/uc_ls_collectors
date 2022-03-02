810eb5a2c46738c69e29a2c0d68c7b6507bc22bb
Ilija Vukotic <ivukotic@cern.ch>
2022-03-01 23:41:45 -0600

    fix, renames

--- filters/latency.rb
+++ filters/latency.rb
@@ -5,22 +5,22 @@ def filter(event)
     med_key=0
     med_val=0
     owds.each do |key, value|
-        kv = key.to_f*value
+        kv = key.to_f
         c = c + value
         sum = sum + kv * value
         if value > med_val
-            med_val=value
-            med_key=kv
+            med_val = value
+            med_key = kv
+        end
     end
     mean=sum/c
     res=0
     owds.each do |key, value|
-        kv = key.to_f*value
-        res = res + kv * (value-mean) ** 2
+        res = res + key.to_f * (value-mean) ** 2
     end
     sd = Math.sqrt(res/c)
-    event.set('delay_mean',mean)
-    event.set('delay_median',med_key)
-    event.set('delay_sd',sd)
+    event.set('delay_mean', mean)
+    event.set('delay_median', med_key)
+    event.set('delay_sd', sd)
     return [event]
 end
