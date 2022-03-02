4a1ef7d16d1796057aa3561d8dad29bce47e53a1
Ilija Vukotic <ivukotic@cern.ch>
2022-03-01 23:10:13 -0600

    addign median, sd

--- filters/latency.rb
+++ filters/latency.rb
@@ -2,11 +2,25 @@ def filter(event)
     owds = event.get('[result][histogram-latency]')
     c = 0
     sum = 0
+    med_key=0
+    med_val=0
     owds.each do |key, value|
-        c=c+value
-        sum=sum+key.to_f*value
+        kv = key.to_f*value
+        c = c + value
+        sum = sum + kv * value
+        if value > med_val
+            med_val=value
+            med_key=kv
     end
-    event.set('owd',sum/c)
-    # puts c, sum, sum/c
+    mean=sum/c
+    res=0
+    owds.each do |key, value|
+        kv = key.to_f*value
+        res = res + kv * (value-mean) ** 2
+    end
+    sd = Math.sqrt(res/c)
+    event.set('delay_mean',mean)
+    event.set('delay_median',med_key)
+    event.set('delay_sd',sd)
     return [event]
 end
