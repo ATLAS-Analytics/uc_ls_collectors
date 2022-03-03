6f6e4014655d0075b17649f801e9cfb91a57fac4
Ilija Vukotic <ivukotic@cern.ch>
2022-03-02 23:04:36 -0600

    fixes

--- filters/trace.rb
+++ filters/trace.rb
@@ -1,23 +1,20 @@
 def filter(event)
-    # hs = event.get('[result][paths]').first
+    hs = event.get('[result][paths]').first()
     c = 1
-    # hops = []
-    # ttls = []
-    # asns = []
-    # rtts = []
-    # hops.push(1)
-    # hops.push(2)
-    # hops.push(3)
-    # hs.each do |h|
-    #     hops.push(h[:ip])
-    #     ttls.push(c)
-    #     asns.push(h[:as][:number])
-    #     rtts.push(h[:rtt])
-    #     c = c + 1
-    # end
-    event.set('hops', c)
-    # event.set('ttls', ttls)
-    # event.set('asns', asns)
-    # event.set('rtts', rtts)
+    hops = []
+    ttls = []
+    asns = []
+    rtts = []
+    hs.each do |h|
+        hops.push(h[:ip])
+        ttls.push(c)
+        asns.push(h[:as][:number])
+        rtts.push(h[:rtt])
+        c = c + 1
+    end
+    event.set('hops', hops)
+    event.set('ttls', ttls)
+    event.set('asns', asns)
+    event.set('rtts', rtts)
     return [event]
 end
