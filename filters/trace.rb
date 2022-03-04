e2822f02b474a66dc3f53223eba344321320e4d4
Ilija Vukotic <ivukotic@cern.ch>
2022-03-03 21:18:19 -0600

    fix for empty hops

--- filters/trace.rb
+++ filters/trace.rb
@@ -6,11 +6,17 @@ def filter(event)
     asns = []
     rtts = []
     hs.each do |h|
-        hops.push(h["ip"])
         ttls.push(c)
-        asns.push(h["as"]["number"])
-        rtts.push(h["rtt"][2,6].to_f)
         c = c + 1
+        if h.length > 0
+            hops.push(h["ip"])
+            asns.push(h["as"]["number"])
+            rtts.push(h["rtt"][2,6].to_f)
+        else
+            hops.push("-")
+            asns.push(0)
+            rtts.push(0)
+        end
     end
     event.set('hops', hops)
     event.set('ttls', ttls)
