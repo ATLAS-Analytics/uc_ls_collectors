08ce10147083357315c7d1ab4b6aefe8bd072fef
Ilija Vukotic <ivukotic@cern.ch>
2022-07-12 11:44:04 -0500

    fix

--- filters/reformat.rb
+++ filters/reformat.rb
@@ -4,12 +4,9 @@ def filter(event)
     readout = event.get("[counters]")
     if readout
         readout.each { |k, v|
-            event.set("[#{k}]", v.value)
+            event.set("[#{k}]", v["value"])
         }
     end
-
-    # event.set("src", get_ips(event.get("src_host")) ) unless event.get("src")
-    # event.set("dest", get_ips(event.get("dest_host"))) unless event.get("dest")
 
     return [event]
 end
