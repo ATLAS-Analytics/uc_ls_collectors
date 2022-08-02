f4fae79ea8c9b7216536cfacbc590e3113d149f6
Ilija Vukotic <ivukotic@cern.ch>
2022-08-02 20:41:33 +0200

    fix to latency rb

--- filters/latency.rb
+++ filters/latency.rb
@@ -1,31 +1,41 @@
 def filter(event)
-    owds = event.get('[result][histogram-latency]')
+    unsorted_owds = event.get('[result][histogram-latency]')
+    owds=Hash[unsorted_owds.sort]
     c = 0
     sum = 0
-    med_key=0
-    med_val=0
     owds.each do |key, value|
         kv = key.to_f
-        c = c + value
+        c += value
         sum = sum + kv * value
-        if value > med_val
-            med_val = value
-            med_key = kv
-        end
     end
+    
     if c>0
+
         mean=sum/c
         res=0
         owds.each do |key, value|
-            res = res + key.to_f * (value-mean) ** 2
+            res += value * (key.to_f-mean) ** 2
         end
         sd = Math.sqrt(res/c)
+
+        co=0
+        med=0
+        c = c/2
+        b.each do |key, value|
+            kv = key.to_f
+            co += value
+            med = kv
+            break if co>c
+        end
+
     else
-        mean=0
-        sd=0
+        mean = 0
+        sd = 0
+        med = 0
     end
+
     event.set('delay_mean', mean)
-    event.set('delay_median', med_key)
+    event.set('delay_median', med)
     event.set('delay_sd', sd)
     return [event]
 end
