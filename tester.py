b24ba88cbaedf2e4ce3cb999258689495f9410aa
Ilija Vukotic <ivukotic@cern.ch>
2022-09-01 12:37:55 -0500

    tester

--- tester.py
+++ tester.py
@@ -2,9 +2,18 @@ import requests
 
 newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-response = requests.post('https://squid.atlas-ml.org', json={'id': 1, 'name': 'Jessa'},
-                         headers=newHeaders)
+data = {
+    'token': 'sqKHBKJB45n',
+    'site': 'MWT2',
+    'squid_id': 'asdf',
+    'timestamp': 1662047737,
+    'hits': 100,
+    'requests': 10,
+    'cputime': 3,
+    'objects': 123,
+    'memory': 1231445
+}
 
+response = requests.post('https://squid.atlas-ml.org', json=data,
+                         headers=newHeaders, verify=False)
 print("Status code: ", response.status_code)
-print("Printing Entire Post Request")
-print(response.json())
