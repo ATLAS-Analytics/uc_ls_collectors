ec509f375a0ec6ee32dee249c5c6256d8720ac7a
Ilija Vukotic <ivukotic@cern.ch>
2022-09-15 16:21:16 -0500

    5959

--- tester.py
+++ tester.py
@@ -4,7 +4,7 @@ import logstash
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
 # test_logger.addHandler(logstash.LogstashHandler('servicex.atlas-ml.org', 5959, version=1))
-test_logger.addHandler(logstash.TCPLogstashHandler('servicex.atlas-ml.org', 443, version=1))
+test_logger.addHandler(logstash.TCPLogstashHandler('servicex.atlas-ml.org', 5959, version=1))
 
 test_logger.error('python-logstash: test logstash error message.')
 test_logger.info('python-logstash: test logstash info message.')
