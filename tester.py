e8562b3630617e84ef4af0b4f3b9050a3dd1a053
Ilija Vukotic <ivukotic@cern.ch>
2022-09-22 10:20:33 -0500

    small

--- tester.py
+++ tester.py
@@ -2,7 +2,8 @@ import logging
 import sys
 import logstash
 
-test_logger = logging.getLogger('python-logstash-logger')
+test_logger = logging.getLogger('test-ilija')
+
 test_logger.setLevel(logging.INFO)
 # test_logger.addHandler(logstash.LogstashHandler('servicex.atlas-ml.org', 5959, version=1))
 test_logger.addHandler(logstash.TCPLogstashHandler('servicex.atlas-ml.org', 5959, version=1))
@@ -17,7 +18,6 @@ metric = {
     'metric_dict': {'a': 1, 'b': 'c'},
     'metric_float': 1.23,
     'metric_integer': 123,
-    'metric_list': [1, 2, '3'],
 }
 
 test_logger.info('python-logstash: test extra fields', extra=metric)
