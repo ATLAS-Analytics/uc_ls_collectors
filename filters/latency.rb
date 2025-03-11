ae7a0abfec3478206795e341c934ef2aa27fbf46
Yana <71765009+hooloobooroodkoo@users.noreply.github.com>
2025-03-11 11:17:15 +0100

    Update latency.rb to calculate and save quantiles and variance

--- filters/latency.rb
+++ filters/latency.rb
@@ -1,41 +1,72 @@
+class Percentile
+  attr_reader :key, :value, :k, :is_calculated
+
+  def initialize(quantile, sample_size)
+    # save 50th quantile as "delay_median" field 
+    @key = quantile == 50 ? "delay_median" : "delay_#{quantile}th_percentile"
+    @k = (quantile / 100.0) * sample_size
+    @value = nil
+    @is_calculated = false
+  end
+
+  def findvalue(curr_count, hist_value)
+    if !@is_calculated && curr_count >= @k
+      @value = hist_value
+      @is_calculated = true
+    end
+  end
+end
+
 def filter(event)
-    unsorted_owds = event.get('[result][histogram-latency]')
-    owds=Hash[unsorted_owds.sort]
-    c = 0
-    sum = 0
+  unsorted_owds = event['result']['histogram-latency']
+  owds = Hash[unsorted_owds.sort]
+  c = 0
+  sum = 0
+  owds.each do |key, value|
+    kv = key.to_f
+    c += value
+    sum += kv * value
+  end
+
+  if c > 0
+    mean = sum / c
+    variance = 0
     owds.each do |key, value|
-        kv = key.to_f
-        c += value
-        sum = sum + kv * value
+      variance += value * (key.to_f - mean) ** 2
     end
-    
-    if c>0
+    # adding variance and standard deviation
+    variance /= c
+    stddev = Math.sqrt(variance)
 
-        mean=sum/c
-        res=0
-        owds.each do |key, value|
-            res += value * (key.to_f-mean) ** 2
+    # adding the calulation of quantiles
+    quantiles = [25, 50, 75, 95]
+    percentiles = quantiles.map { |q| Percentile.new(q, c) }
+    percentile = percentiles.shift
+    curr_count = 0
+    owds.sort.each do |key, value|
+      hist_value = key.to_f
+      curr_count += value
+      # percentiles
+      while percentile && curr_count >= percentile.k
+        percentile.findvalue(curr_count, hist_value)
+        if percentile.is_calculated
+            # saving quantiles
+          event.set(percentile.key, percentile.value)
+          percentile = percentiles.shift
+        else
+          break
         end
-        sd = Math.sqrt(res/c)
-
-        co=0
-        med=0
-        c = c/2
-        owds.each do |key, value|
-            kv = key.to_f
-            co += value
-            med = kv
-            break if co>c
-        end
-
-    else
-        mean = 0
-        sd = 0
-        med = 0
+      end
     end
 
-    event.set('delay_mean', mean)
-    event.set('delay_median', med)
-    event.set('delay_sd', sd)
-    return [event]
+  else
+    mean = 0
+    stddev = 0
+    variance = 0
+  end
+  # saving other statistics
+  event.set('delay_mean', mean)
+  event.set('delay_sd', stddev)
+  event.set('delay_variance', variance)
+  return event
 end
