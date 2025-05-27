0c588079275ee2b3890ad614b8d6f8e3fed2c67d
Ilija Vukotic <ivukotic@uchicago.edu>
2025-05-27 18:16:39 -0500

    Update VarnishMonitor workflow and configurations for varnish-accesses

--- filters/reformat.rb
+++
@@ -1,9 +0,0 @@
-def filter(event)
-    readout = event.get("[counters]")
-    if readout
-        readout.each { |k, v|
-            event.set("[#{k}]", v["value"])
-        }
-    end
-    return [event]
-end
