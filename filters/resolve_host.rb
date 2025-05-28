c60da47158dac28c9081d5786063102b5fc3f193
Ilija Vukotic <ivukotic@uchicago.edu>
2025-05-28 00:33:22 -0500

    Refactor memcached configuration in varnish.conf and update client identification in resolve_host.rb

--- filters/resolve_host.rb
+++ filters/resolve_host.rb
@@ -1,17 +1,14 @@
 require 'socket'
 
 def lookup_hostname(ip)
-    result = {
-        'client' => ''   
-    }
     begin
-        result['client'] = Socket.gethostbyaddr(IPAddr.new(ip).hton).first
+        return Socket.gethostbyaddr(IPAddr.new(ip).hton).first
     rescue SocketError
     end
-    reurn result
+    reurn nil
 end
 
 def filter(event)
-    event.set("source", lookup_hostname(event.get("ip")) ) unless event.get("source")
+    event.set("client", lookup_hostname(event.get("ip")) ) unless event.get("client")
     return [event]
 end
